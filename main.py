780deacf1116939d4670c91951bc4e1d46baca4d
coffee2boss <coffee2boss@gmail.com>
2022-03-31 15:24:54 +0100

    SNAKE CREATED AS CLASS AND MOVEMENT CONTROLS ADDED

--- main.py
+++ main.py
@@ -1,61 +1,26 @@
-# # Extracting colors from an art image and creating rgb colors inside an empty list.
-# import colorgram
-
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg',30)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color =(r,g,b)
-#     rgb_colors.append(new_color)
-
-#all extracted colors that we want to use inside a new list called color_list in the format we need.
-color_list = [(134, 167, 191), (211, 156, 106), (197, 143, 166), (29, 110, 168), (234, 214, 86), (127, 74, 92), (187, 178, 19), (26, 136, 66), (55, 18, 26), (142, 20, 40), (38, 175, 113), (225, 170, 
-196), (116, 188, 144), (231, 77, 50), (172, 70, 46), (238, 218, 5), (37, 17, 16), (186, 91, 110), (9, 102, 52), (34, 167, 189), (20, 20, 28), (183, 185, 213), (234, 171, 159), (154, 215, 172), (142, 20, 16), (89, 124, 182)]
-
+import time
 from turtle import Screen, Turtle
-import turtle as t
-import random 
+from snake import Snake
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("SNAKE GAME")
+screen.tracer(0)
 
-p = Turtle()
-t.colormode(255)
-p.speed("fastest")
-p.hideturtle()
-p.width(20)
+snake = Snake()
 
-x_cord = -280
-y_cord = -280
+screen.listen()
+screen.onkey(snake.up, "Up")                        # input and ties to the controls for the snake
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-def random_color():
-    random_colors = random.choice(color_list)
-    return random_colors
+#get snake to automatically move forwards
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-def X_line():
-    for X_dot in range(10):
-        p.pencolor(random_color())
-        p.dot()
-        p.penup()
-        p.forward(60)
-        p.pendown
+    snake.move()
 
-def Y_line():
-    global y_cord
-    for _ in range(2):
-        new_y_coord = int(y_cord + 30)
-        print(new_y_coord)
-        p.penup()
-        p.goto(x_cord, new_y_coord)
-        y_cord = new_y_coord
-        p.pendown
-
-for item in range(8):
-    p.penup()
-    p.goto(-280,0)
-    p.pendown
-    Y_line()
-    X_line()
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,51 @@
+# snake class for snake project
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
+#create initial snake body at initial position
+class Snake:
+    """Models each snake segment."""
+    def __init__(self):
+        self.segments = []                          # bundeling all parts of snake together. 
+        self.create_snake()
+        self.head = self.segments[0]                # DECLARING WHICH PART OF SNAKE IS THE HEAD
+
+    def create_snake(self): 
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("green")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+             
+#get snake to automatically move forwards together
+    def move(self):
+        for seg_num in range(len(self.segments) -1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    # controls for the snake
+    def right(self):                        
+        if self.head.heading() != LEFT:         # this stops the snake going back on itself
+            self.head.setheading(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN:      
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:    
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:    
+            self.head.setheading(LEFT)
+
